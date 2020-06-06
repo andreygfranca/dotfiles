98f4aff6e3146de8409df88e5042e1899d70d9f5
Andrey França <andrey.franca@totvs.com.br>
2020-06-05 22:36:49 -0300

    adding i3 and rofi configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,7 @@ ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/zprofile ~/.zprofile
 ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sr ~/dotfiles/config/i3 ~/.config/
+ln -sr ~/dotfiles/config/rofi ~/.config/
 
 source ~/.zshrc
