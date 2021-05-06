bf724292638bac475c9bf5139f5c24383dd3bc1c
Andrey Franca <andrey.franca@zgsolucoes.com.br>
2021-05-06 18:11:50 -0300

    feat: addin aliases to npm

--- aliases/npm.sh
+++ aliases/npm.sh
@@ -3,6 +3,7 @@ alias ni="npm i"
 alias nci="npm ci"
 alias nrd="npm run dev"
 alias nrs="npm run start"
+alias nrb="npm run build"
 
 # Links
 alias nl="npm link"
@@ -10,5 +11,13 @@ alias nlac="npm link @zg-angular/commons"
 alias nlbrc="npm link @zg-angular/bills-reviewer-client"
 alias nlsrc="npm link @zg-angular/settings-repository-client"
 
-# Examples
-alias nbase="ni && nl && nlac"
+# completes
+alias nbase="ni && nl && nrb"
+alias nbase2="ni && nl && nlac"
+alias nbasec="nbase2 && nrb"
+
+# applications
+alias zggc="ni && nlac && nlbrc && nrd"
+
+# go
+alias gogc="cd /media/WORK/zeroglosa-workspace/faturamento-eficiente/gerenciamento-criticas-ui"
