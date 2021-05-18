385981b5739229b47ada5438795b21a7c5cacb64
Andrey Franca <andrey.franca@zgsolucoes.com.br>
2021-05-18 15:16:23 -0300

    refactor: change zggz alias adding nlbac

--- aliases/npm.sh
+++ aliases/npm.sh
@@ -10,6 +10,7 @@ alias nl="npm link"
 alias nlac="npm link @zg-angular/commons"
 alias nlbrc="npm link @zg-angular/bills-reviewer-client"
 alias nlsrc="npm link @zg-angular/settings-repository-client"
+alias nlbac="npm link @zg-angular/banco-arquivos-client"
 
 # completes
 alias nbase="ni && nl && nrb"
@@ -17,7 +18,7 @@ alias nbase2="ni && nl && nlac"
 alias nbasec="nbase2 && nrb"
 
 # applications
-alias zggc="ni && nlac && nlbrc && nrd"
+alias zggc="ni && nlac && nlbrc && nlbac && nrd"
 
 # go
 alias gogc="cd /media/WORK/zeroglosa-workspace/faturamento-eficiente/gerenciamento-criticas-ui"
