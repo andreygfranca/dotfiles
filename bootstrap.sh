91938cb9fd5ffd2d7ac35e468aa1d4d01685197f
andreygfranca <andreygfranca@gmail.com>
2021-07-19 18:13:19 -0300

    feat: add zsh-autosuggestion plugin

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,14 @@ then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+#install tmux plugin manager
 if [ ! -d "~/.tmux/plugins/tpm" ]
 then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
+
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # link files to correct destination
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
