a7f542a6adf137fcd515ba75697495c4d139e8c8
andreygfranca <andreygfranca@gmail.com>
2021-07-23 15:57:50 -0300

    refactor: removing zg npm alises

--- aliases/npm.sh
+++ aliases/npm.sh
@@ -7,21 +7,11 @@ alias nrb="npm run build"
 
 # Links
 alias nl="npm link"
-alias nlac="npm link @zg-angular/commons"
-alias nlbrc="npm link @zg-angular/bills-reviewer-client"
-alias nlsrc="npm link @zg-angular/settings-repository-client"
-alias nlbac="npm link @zg-angular/banco-arquivos-client"
 
 # completes
 alias nbase="ni && nl && nrb"
 alias nbase2="ni && nl && nlac"
 alias nbasec="nbase2 && nrb"
-
-# applications
-alias zggc="ni && nlac && nlbrc && nlbac && nrd"
-
-# go
-alias gogc="cd /media/WORK/zeroglosa-workspace/faturamento-eficiente/gerenciamento-criticas-ui"
 
 alias nclean="rm -rf __ivy__ngcc && git checkout package.json"
 
